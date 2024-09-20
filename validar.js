1441cac88f0e455365f74adf57cc9884e2d96c5e
GabSC-07 <serratos.cortes.gabriel@gmail.com>
2024-09-20 10:55:32 -0600

    first commit

---
+++ validar.js
@@ -0,0 +1,34 @@
+function validar(form){
+    
+    //validar nombre
+    let nombre=form.nombre.value.trim()
+    if(nombre==""){
+        alert("Por favor, ingresa un nombre válido")
+        return false
+    }
+
+    //validar edad
+    let edad=form.edad.value.trim()
+    if(edad===""|| isNaN(edad)|| edad<=0){
+        alert("Por favor, ingrese una edad válida")
+        return false
+    }
+
+    //validar sexo
+    let sexo=form.sexo.value
+    if(sexo==""){
+        alert("Por favor, ingrese un sexo")
+        return false
+    }
+
+    //validar deporte
+    let deporte=form.deporte.value
+    if(deporte=="ninguno"){
+        alert("Por favor, ingrese un deporte")
+        return false
+    }
+
+    //si todo es válido
+    alert("Datos Correctos")
+    return true
+}
